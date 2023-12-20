a331ad78d70910f664bfb732d4f97165f2f6b797
dilettits <mantovani.d3@gmail.com>
2023-12-20 11:02:51 +0100

    created form.php and password.php

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Password Generator</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>PW Gen</h1>
+                <form>
+                    <label for="pwd_length"></label>
+                    <input id="pwd_length" name="pwd_length" type="number" placeholder="Lunghezza">
+                    <button type="submit" href="#" class="btn-primary">Generate PW</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
